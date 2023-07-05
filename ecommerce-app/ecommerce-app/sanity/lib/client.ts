2621048999ada217be2e855de38803d7625edb8d
NonsoBarn <102404755+NonsoBarn@users.noreply.github.com>
2023-07-05 11:20:57 +0100

    final

--- ecommerce-app/ecommerce-app/sanity/lib/client.ts
+++ ecommerce-app/ecommerce-app/sanity/lib/client.ts
@@ -7,6 +7,5 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token:
-    "skJ1M7gxMXLO9CRmKE5NaeZpZwisEGu8S2PMVkZCnHEGbJDGIJpp0hS0jOZ4qic4IN7eYpANrkrvU1Sthcpm5M3YlgXzC2LyNUPv0ohU9AuB5ZtyypNGclKqO4n1i6QyMWFrEPYsbp8OBh2LZXc0q8YraxtbaC3X9Q2mhfdNiPxN10MW1uZw",
+  token: "",
 })
